2b71c47d9acdd961c96e0da03266c8fdd6e1c883
OliverDaviesCodes <oliverdavieswork@gmail.com>
2021-10-02 10:03:55 +0000

    updated typo in app.py

--- app.py
+++ app.py
@@ -2,3 +2,19 @@ import os
 from flask import Flask
 if os.path.exists("env.py"):
     import env
+
+app = Flask(__name__)
+
+
+@app.route("/")
+def hello():
+    return "Hello World ... again"
+
+# This tells the app how and where to run the application
+
+
+if __name__ == "__main__":
+    app.run(host=os.environ.get("IP"),
+            port=int(os.environ.get("PORT")),
+            # make sure to turn this to false for deployment
+            debug=True)
