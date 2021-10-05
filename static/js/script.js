67b00f028818abe1e424d25633ffcfc6aa5bc051
OliverDaviesCodes <oliverdavieswork@gmail.com>
2021-10-05 10:07:52 +0000

    Add Task input fields

--- static/js/script.js
+++ static/js/script.js
@@ -2,12 +2,14 @@ $(document).ready(function () {
     $('.sidenav').sidenav({
         edge: "right"
     });
-});
-
-$(document).ready(function () {
+    $('.datepicker').datepicker({
+        format: "dd mmmm, yyyy",
+        yearRange: 3,
+        showClearBtn: true,
+        i18n: {
+            done: "select"
+        }
+    });
     $('.collapsible').collapsible();
-});
-
-$(document).ready(function () {
     $('.tooltipped').tooltip();
 });
