3fe79b6198139b36d963d0bc87e6c3c1201239a1
OliverDaviesCodes <oliverdavieswork@gmail.com>
2021-10-13 08:55:19 +0000

    Add Task edit and done buttons

--- app.py
+++ app.py
@@ -112,7 +112,7 @@ def add_task():
         mongo.db.tasks.insert_one(task)
         flash("Task Successfully Added")
         return redirect(url_for("get_tasks"))
-        
+
     categories = mongo.db.categories.find().sort("category_name", 1)
     return render_template("add_task.html", categories=categories)
 
